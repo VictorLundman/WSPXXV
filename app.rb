d98d2938c82cf06e20042aa2306def3bcb3b8cca
VictorLundman <186269292+VictorLundman@users.noreply.github.com>
2026-02-05 16:49:52 +0100

    v6

--- app.rb
+++ app.rb
@@ -3,3 +3,83 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
+
+before do
+  user_id = session[:user_id]
+  if user_id == nil
+      @user = nil
+      return
+  end
+
+  db = connectToDb()
+  user = db.execute("SELECT id, username FROM users WHERE id=?", [user_id])
+  if user.empty?
+      @user = nil
+      return
+  end
+
+  @user = user[0]
+end
+
+def connectToDb()
+  db = SQLite3::Database.new("db/databas.db")
+  db.results_as_hash = true
+
+  return db
+end
+
+get "/" do
+  db = connectToDb()
+  @categories = db.execute("SELECT id, name FROM category")
+
+  slim(:index)
+end
+
+get "/categories/:id" do
+  id = params[:id].to_i
+  db = connectToDb()
+
+  categories = db.execute("SELECT * FROM category WHERE id = ?", [id])
+  if categories.empty?
+    error(404)
+  end
+
+  @category = categories.first
+  @threads = db.execute("SELECT * FROM thread WHERE category_id = ? ORDER BY created DESC", [id])
+
+  slim(:"categories/view")
+end
+
+get "/threads/:id" do
+  id = params[:id].to_i
+  db = connectToDb()
+
+  threads = db.execute("SELECT * FROM thread WHERE id = ?", [id])
+  if threads.empty?
+    error(404)
+  end
+
+  @thread = threads.first
+  @replies = db.execute("SELECT * FROM reply where thread_id = ? ORDER BY created, id ASC", [id])
+
+  slim(:"threads/view")
+end
+
+post "/threads/:id" do
+  id = params[:id].to_i
+  db = connectToDb()
+
+  threads = db.execute("SELECT * FROM thread WHERE id = ?", [id])
+  if threads.empty?
+    error(404)
+  end
+
+  content = params[:content]
+  if content.empty?
+    error(400)
+  end
+
+  db.execute("INSERT INTO reply (thread_id, content, owner_id) VALUES (?, ?, ?)", [id, content, 1])
+
+  redirect("/threads/#{id}")
+end
--- db/seeder.rb
+++ db/seeder.rb
@@ -4,7 +4,6 @@ db = SQLite3::Database.new("databas.db")
 
 
 def seed!(db)
-  puts "Using db file: db/todos.db"
   puts "🧹 Dropping old tables..."
   drop_tables(db)
   puts "🧱 Creating tables..."
@@ -15,21 +14,51 @@ def seed!(db)
 end
 
 def drop_tables(db)
-  db.execute('DROP TABLE IF EXISTS exempel')
+  db.execute('DROP TABLE IF EXISTS user')
+  db.execute('DROP TABLE IF EXISTS category')
+  db.execute('DROP TABLE IF EXISTS thread')
+  db.execute('DROP TABLE IF EXISTS reply')
 end
 
 def create_tables(db)
-  db.execute('CREATE TABLE exempel (
+  db.execute('CREATE TABLE user (
+              id INTEGER PRIMARY KEY AUTOINCREMENT)')
+  db.execute('CREATE TABLE category (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL, 
-              description TEXT,
-              state BOOLEAN)')
+              name TEXT NOT NULL)')
+  db.execute('CREATE TABLE thread (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              category_id INTEGER NOT NULL,
+              title TEXT NOT NULL,
+              content TEXT NOT NULL,
+              owner_id INTEGER NOT NULL,
+              created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+              FOREIGN KEY(category_id) REFERENCES category(id) ON DELETE CASCADE,
+              FOREIGN KEY(owner_id) REFERENCES user(id) ON DELETE CASCADE)')
+  db.execute('CREATE TABLE reply (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              thread_id INTEGER NOT NULL,
+              content TEXT NOT NULL,
+              owner_id INTEGER NOT NULL,
+              created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+              FOREIGN KEY(thread_id) REFERENCES thread(id) ON DELETE CASCADE,
+              FOREIGN KEY(owner_id) REFERENCES user(id) ON DELETE CASCADE)')
 end
 
 def populate_tables(db)
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp mjölk", "3 liter mellanmjölk, eko",false)')
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp julgran", "En rödgran",false)')
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Pynta gran", "Glöm inte lamporna i granen och tomten",false)')
+  db.execute("INSERT INTO user DEFAULT VALUES")
+
+  category_id_1 = db.execute("INSERT INTO category (name) VALUES (?) RETURNING id", ["Roblox"])[0][0]
+  category_id_2 = db.execute("INSERT INTO category (name) VALUES (?) RETURNING id", ["Fortnite"])[0][0]
+
+  roblox_thread_id_1 = db.execute("INSERT INTO thread (category_id, title, content, owner_id) VALUES (?, ?, ?, ?) RETURNING id", [category_id_1, "Varför är Roblox bättre än Fortnite?", "Hjälp mig!", 1])[0][0]
+  db.execute("INSERT INTO reply (thread_id, content, owner_id) VALUES (?, ?, ?)", [roblox_thread_id_1, "Har du någonsin testat Fortnite?", 2])
+  db.execute("INSERT INTO reply (thread_id, content, owner_id) VALUES (?, ?, ?)", [roblox_thread_id_1, "Nej, vadå?", 1])
+  db.execute("INSERT INTO reply (thread_id, content, owner_id) VALUES (?, ?, ?)", [roblox_thread_id_1, "Om du hade testat att spela Fortnite någon gång hade du märkt att det är bäst, kan man ens använda emotes i Roblox?", 2])
+  db.execute("INSERT INTO reply (thread_id, content, owner_id) VALUES (?, ?, ?)", [roblox_thread_id_1, "Ja, mycket bättre emotes. Fortnite är sämst!", 1])
+
+  fortnite_thread_id_1 = db.execute("INSERT INTO thread (category_id, title, content, owner_id) VALUES (?, ?, ?, ?) RETURNING id", [category_id_2, "Roblox är bättre!", "Det är sant!", 1])[0][0]
+  db.execute("INSERT INTO reply (thread_id, content, owner_id) VALUES (?, ?, ?)", [fortnite_thread_id_1, "Jag kanske borde testa Roblox", 2])
 end
 
 
